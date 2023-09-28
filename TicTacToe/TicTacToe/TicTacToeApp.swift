046428663f0c944c1057149f022a4e83c9525b8a
Nick Theodoridis <122683142+nicktheodoridisiOS@users.noreply.github.com>
2023-09-28 14:13:29 +0300

    Create xCode Project

---
+++ TicTacToe/TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Nick Theodoridis on 28/9/23.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
